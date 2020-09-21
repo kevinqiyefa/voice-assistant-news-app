cd368560089757309337785e5f3cd10a0cb883eb
Kevin <kevinqiyf@hotmail.com>
2020-09-20 22:25:14 -0700

    add default instruction cards

--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -5,4 +5,20 @@ export default makeStyles({
     width: '100%',
     margin: 0,
   },
+  infoCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '45vh',
+    padding: '10%',
+    borderRadius: 10,
+    color: 'white',
+  },
 });
