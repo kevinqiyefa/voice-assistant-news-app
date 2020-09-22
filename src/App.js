8ad6a5d63eae8626de02d1e021649699df87236a
Kevin <kevinqiyf@hotmail.com>
2020-09-21 17:36:18 -0700

    opening article feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import { InstructionModal, NewsCards } from './components';
 import aiLogo from './assets/images/ai-logo.gif';
 import useStyles from './styles';
@@ -65,23 +66,56 @@ function App() {
 
   const classes = useStyles();
 
+  const parsedNumber = (number) => {
+    if (number.length > 2) {
+      //wordsToNumbers fuzzy search cannot convert these words to number
+      if (number === 'free') {
+        return 3;
+      } else if (number === 'fi') {
+        return 5;
+      }
+      return wordsToNumbers(number, { fuzzy: true });
+    } else {
+      return +number;
+    }
+  };
+
   useEffect(() => {
     alanBtn({
       key: ALAN_SDK_KEY,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         switch (command) {
           case 'newHeadlines':
             setNewsArticles(articles);
             setActiveArticle(-1);
             break;
           case 'showInstructions':
+            alanBtn().playText('Here are the instructions.');
             setShowInstructions(true);
             break;
           case 'hideInstructions':
+            alanBtn().playText('Sure, hiding the instructions.');
             setShowInstructions(false);
             break;
           case 'highlight':
             setActiveArticle((prevA) => prevA + 1);
+            break;
+          case 'openArticle':
+            const n = parsedNumber(number);
+
+            const article = articles[n - 1];
+
+            if (n <= articles.length && article) {
+              window.open(article.url, '_blank');
+              alanBtn().playText('Opening...');
+            } else {
+              if (articles.length < 1) {
+                alanBtn().playText('Please search the news articles first...');
+              } else {
+                alanBtn().playText('Please try that again...');
+              }
+            }
+
             break;
           default:
         }
